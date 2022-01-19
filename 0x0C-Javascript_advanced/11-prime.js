b5a63c61e120f2d5a40782f2658d28be445ec8e0
Felipe Prado <jfpc11@misena.edu.co>
2022-01-19 17:14:11 -0500

    executing the primes at the end of the stack

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+  let count = 0;
+  for (let prime = 2; prime <= 100; prime++) {
+    let isPrime = true;
+    for (let i = 2; i < prime; i++) {
+      if (prime % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      count++;
+    }
+  }
+  return count;
+}
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+  setTimeout(function cb() {
+    countPrimeNumbers();
+  }, 0);
+}
+const t1 = performance.now();
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    t1 - t0
+  } milliseconds.`
+);
